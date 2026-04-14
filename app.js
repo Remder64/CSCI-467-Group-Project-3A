d47b1411e303fe8df528c7cfa8d23aebf61f59ec
Max Palomar <149902313+Remder64@users.noreply.github.com>
2026-04-13 23:06:49 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
 var express = require('express');
 var app = express();
+//var bodyParser = require('body-parser');
+//var multer = require('multer');
+//var upload = multer();
+const data = require('./controllers/credit');
 const port = 4010;
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-
-
 app.get('/', function(req, res) {
   res.render('main');
 });
 
+
+//part webpage
 const parts = require('./controllers/parts');
 app.get('/getParts', (req, res) => {
   parts.getAll((list) => {
     res.render('parts.ejs', { all: list });
+
   });
 });  
 
+app.get('/getParts/item', (req, res) => {
+  res.send('This will be a item, you can buy it eventually');
+})
+
+//credit webpage
 const credit = require('./controllers/credit');
 app.get('/processCC', (req, res) => {
   credit.processSample((result) => {
@@ -25,12 +35,9 @@ app.get('/processCC', (req, res) => {
   });
 })
 
-
-app.all('/secret', (req, res, next) => {
-    res.send('They took my entire life savings');
-    console.log('Time: ', Date.now());
-    next();
-});
+app.get('/', (req, res) => {
+  console.log("A new request received at " + Date().now());
+})
 
 
 app.listen(port, () => {
