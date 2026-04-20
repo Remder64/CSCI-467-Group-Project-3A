75029387c0a6bb5c0c9a335d42f3c164a1912632
Max Palomar <149902313+Remder64@users.noreply.github.com>
2026-04-19 21:09:38 -0500

    Update parts.js

--- controllers/parts.js
+++ controllers/parts.js
@@ -1,5 +1,5 @@
 var mysql = require('mysql')
-var connection = mysql.createConnection({
+var pool = mysql.createPool({
   host: 'blitz.cs.niu.edu',
   user: 'student',
   password: 'student',
@@ -8,16 +8,27 @@ var connection = mysql.createConnection({
   ssl: true
 });
 
-//hi
-
-connection.connect();
-
 module.exports = {
     getAll: async result => {
-        connection.query('SELECT * FROM parts', function(err, rows){
+        pool.query('SELECT * FROM parts', function(err, rows){
             if (err) throw err;
             console.log('found ', rows.length , ' parts');
             result(rows);
         });
+    },
+
+    getByNum: async (partNum, result) => {
+        pool.query('SELECT * FROM parts WHERE number = ?', [partNum], function(err, rows) {
+            if(err) throw err;
+            result(rows[0]);
+        })
+    },
+
+    searchAll: async (searchStr, result) => {
+        pool.query('SELECT * FROM parts WHERE description LIKE ?', ['%' + searchStr + '%'], function(err, rows) {
+            if(err) throw err;
+            console.log('found ', rows.length , ' parts');
+            result(rows);
+        })
     }
 }
