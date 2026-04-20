c182160dc9ca05e53af361da33f24072c15c27dc
Max Palomar <149902313+Remder64@users.noreply.github.com>
2026-04-19 21:10:03 -0500

    Update credit.js

--- controllers/credit.js
+++ controllers/credit.js
@@ -2,21 +2,48 @@ const axios = require('axios');
 const express = require('express');
 var app = express();
 
-app.get('./processCC', function(req, res) {
-    res.send("Hi, this is where you process credit cards")
-})
+//global variables
+let currentRandChars = randCharGenerator();
+let counter = 1;
 
+function transIdGenerator() {
+    if( counter > 9999 ) {
+        currentRandChars = randCharGenerator();
+        counter = 1;
+    }
+
+    var padding = String(counter).padStart(4, '0');
+
+    const transID = "MAP-" + currentRandChars + "-" + padding;
+
+    return transID;
+}
+
+function randCharGenerator() {
+    var chars = 'abcdefghjkmnpqrtuvwxyzABCDEFGHJKMNPQRTUVWXYZ0123456789'
+    var randID = '';
+
+    for( let i = 0; i < 8; i++ ) {
+        const randChars = Math.floor(Math.random() * chars.length);
+        randID += chars.charAt(randChars);
+    }
+
+    return randID;
+}
 
 
 module.exports = {
-    processSample: async result => {
+    processTrans: async (item, result) => {
+
+        const transID = transIdGenerator();
+
         var data = {
-            'vendor': 'VE001-99',
-            'trans': '907-987654320-296',
-            'cc': '6011 1234 4321 1234',
-            'name': 'John Doe', 
-            'exp': '12/2029', 
-            'amount': '654.32'
+            'vendor': 'MANual Auto & Parts',
+            'trans': transID,
+            'cc': item.cc,
+            'name': item.name, 
+            'exp': item.exp, 
+            'amount': item.amount.toFixed(2)
         };
         axios.post('http://blitz.cs.niu.edu/creditcard', data).then((response) => {           
             result(response.data);
