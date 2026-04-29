b17ecb6b0a1dc6d115ae40f6324fbfc42843af20
RobeyTecson <robberyrobey@gmail.com>
2026-04-29 16:56:11 -0500

    Add files via upload
    Copies some info from the legacy database into the new database

---
+++ tempcode/copy.js
@@ -0,0 +1,44 @@
+
+const mysql = require('mysql2/promise');
+
+//cloud
+const pool1 = mysql.createPool({
+    host: 'blitz.cs.niu.edu',
+    user: 'student',
+    password: 'student',
+    database: 'csci467',
+    port: '3306',
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+//local
+const pool2 = mysql.createPool({
+    host: 'localhost',
+    user: 'guest',
+    password: 'guest123',
+    database: 'auto_parts',
+    port: '3306'
+});
+
+async function copyParts() {
+    try {
+        const [rows] = await pool1.query('SELECT number, description FROM parts');
+
+        console.log(`Fetched ${rows.length} parts`);
+
+        for (const autoParts of rows) {
+            await pool2.query('INSERT INTO inventory (partNumber, quantityOnHand, description) VALUES (?,0,?)',
+                [autoParts.number, autoParts.description]);
+        }
+
+        console.log('Data copied successfully!');
+    }
+
+    catch(err) {
+        console.error(err);
+
+    }
+}
+
+copyParts();
