fa42ec16fd8e93828de301e11e3486364d2f802b
RobeyTecson <robberyrobey@gmail.com>
2026-04-29 16:58:08 -0500

    Add files via upload
    run this sql locally to make database

---
+++ tempcode/productSystemTables.sql
@@ -0,0 +1,19 @@
+Create database auto_parts;
+use auto_parts;
+
+-- run this to create tables
+
+CREATE TABLE customerorders (
+	orderid int not null primary key auto_increment,
+    date date,
+    time time,
+    status varchar(15) NOT NULL,
+    price float(4,2) NOT NULL,
+    weightLB float(4,2) NOT NULL
+);
+
+create table inventory (
+	partNumber int primary key,
+    quantityOnHand int,
+    description varchar(50)
+);
