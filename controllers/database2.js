51ab43a68677f9b864c09f4516097544fa331bcb
RobeyTecson <robberyrobey@gmail.com>
2026-04-30 16:55:10 -0500

    Update database2.js
    added some documentation and functions

--- controllers/database2.js
+++ controllers/database2.js
@@ -1,5 +1,3 @@
-
-
 var mysql = require('mysql2')
 var pool2 = mysql.createPool({
     host: 'localhost',
@@ -11,10 +9,42 @@ var pool2 = mysql.createPool({
 
 
 module.exports = {
+    //get all info on parts in inventory
     partID: async result => {
         pool2.query('SELECT * FROM inventory', function(err, rows){
             if (err) throw err;
-            console.log('found ', rows.length , ' parts in inventory');
+            console.log('found ', rows.length , ' parts');
+            result(rows);
+        });
+    },
+
+    //changes the quantity on hand for a specific part
+    ChangeQuantity: (partNumber, quantity, result) => {
+        pool2.query(
+            'UPDATE inventory SET quantityOnHand = ? WHERE partNumber = ?',
+            [quantity, partNumber],
+            function (err, rows) {
+                if (err) throw err;
+                console.log('changed ', rows.affectedRows, ' row in inventory');
+                result(rows);
+            }
+        );
+    },
+
+    //get all info on customer orders
+    custOrderID: async result => {
+        pool2.query('SELECT * FROM customerorders', function (err, rows) {
+            if (err) throw err;
+            console.log('found ', rows.length, ' customer orders');
+            result(rows);
+        });
+    },
+
+    //get all parts on a specific order
+    orderPartID: async (orderID, result) => {
+        pool2.query('SELECT * FROM orderParts WHERE orderID = ?', function (err, rows) {
+            if (err) throw err;
+            console.log('found ', rows.length, ' customer orders');
             result(rows);
         });
     },
